907e958fb11f0c13d42fa4e22bd8ea9a1c6583f6
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-08-13 19:49:30 +0700

    217 solution with HashMap 1 pass and 2 pass

--- leetcode/src/LC_1_Two_Sum.java
+++ leetcode/src/LC_1_Two_Sum.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class LC_1_Two_Sum {
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC_1_Two_Sum {
+    public int[] twoSum_Brute_Force(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[]{i, j};
+            }
+        }
+        return new int[]{};
+    }
+
+    public int[] twoSumTwoPass(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            numMap.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement) && numMap.get(complement) != i)
+                return new int[]{i, numMap.get(complement)};
+        }
+        return new int[]{};
+    }
+
+    public int[] twoSumOnePass(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement))
+                return new int[]{i, numMap.get(complement)};
+            numMap.put(nums[i], i);
+        }
+        return new int[]{};
+    }
 }
