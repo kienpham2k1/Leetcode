8d2cbf3b784440a28e36226242bf0688976c6b25
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-08-13 17:32:45 +0700

    217 solution with HashSet

--- leetcode/src/LC_217_Contains_Duplicate.java
+++ leetcode/src/LC_217_Contains_Duplicate.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class LC_217_Contains_Duplicate {
     public boolean containsDuplicate(int[] nums) {
@@ -21,6 +22,15 @@ public class LC_217_Contains_Duplicate {
         Arrays.sort(nums);
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] == nums[i - 1]) return true;
+        }
+        return false;
+    }
+
+    public boolean containsDuplicateWithSHashSet(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (seen.contains(nums[i])) return true;
+            seen.add(nums[i]);
         }
         return false;
     }
