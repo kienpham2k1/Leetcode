1314118b9093ed428cca87fae11128d67d66f2fa
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-08-13 17:22:07 +0700

    push 1st solution 217

--- leetcode/src/LC_217_Contains_Duplicate.java
+++ leetcode/src/LC_217_Contains_Duplicate.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class _217_Contains_Duplicate {
+public class LC_217_Contains_Duplicate {
+    public boolean containsDuplicate(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) return true;
+            else if (nums[i] < nums[i - 1]) {
+                for (int j = i - 1; j >= 0; j--) {
+                    if (nums[j] == nums[i]) return true;
+                }
+                int temp = nums[i];
+                nums[i] = nums[i - 1];
+                nums[i - 1] = temp;
+            }
+        }
+        return false;
+    }
 }
