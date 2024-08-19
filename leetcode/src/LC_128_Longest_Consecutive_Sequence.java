ec300f95e87d7b3b62328699b2821f2253a96bbe
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-08-19 23:54:48 +0700

    238. Product of Array Except Self 128. Longest Consecutive Sequence

---
+++ leetcode/src/LC_128_Longest_Consecutive_Sequence.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class LC_128_Longest_Consecutive_Sequence {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        Arrays.sort(nums);
+        int longest = 1;
+        int length = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] + 1 == nums[i]) {
+                length++;
+            } else if (nums[i - 1] == nums[i])
+                continue;
+            else {
+                length = 1;
+            }
+            longest = Math.max(length, longest);
+        }
+        return longest;
+    }
+}
