b2e6faaae2d522db7eb605d6b8fb02db745f188f
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-08-26 20:08:14 +0700

    125 128 167 217

---
+++ leetcode/src/LC_167_Two_Sum_II_Input_Array_Is_Sorted.java
@@ -0,0 +1,16 @@
+public class LC_167_Two_Sum_II_Input_Array_Is_Sorted {
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.length - 1;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum > target) {
+                r--;
+            } else if (sum < target) {
+                l++;
+            } else {
+                return new int[]{l + 1, r + 1};
+            }
+        }
+        return new int[]{};
+    }
+}
